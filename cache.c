726c1356da255727d301b4c4e441738e9b9df0b8
semvlu <74308718+semvlu@users.noreply.github.com>
2021-12-24 14:52:18 +0800

    Update cache.c

--- cache.c
+++ cache.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-
-int main()
+double cache(int objsize,
+int Rdatabwsr, // avg data rate to browser
+int RTT,
+int Raclink, // access link rate
+int a, // proxy switch
+float Dinternet, // Internet delay
+float Dlan, // LAN delay, ms to s
+float Dcache);
+   
+double cache(int objsize,
+int Rdatabwsr, // avg data rate to browser
+int RTT,
+int Raclink, // access link rate
+int a, // proxy switch
+float Dinternet, // Internet delay
+float Dlan, // LAN delay, ms to s
+float Dcache)
 {
+    /*
     int objsize = 1000000;
     int Rdatabwsr = 15000000; // avg data rate to browser
     int RTT = 2;
@@ -13,16 +29,20 @@ int main()
     float Dinternet = 2.0; // Internet delay
     float Dlan = 0.003; // LAN delay, ms to s
     float Dcache = 0.001; // chche delat
+    */
+    
+    float lan; // LAN util
+    float aclink; // Access Link util
+    float D; // total delay
 
-    puts("Enter 0: w/o proxy, 1: w/ proxy: ");
-    scanf("%d", &a);
+    // Enter 0: w/o proxy, 1: w/ proxy
 
     if (a == 0)
     {
         lan = Rdatabwsr / 100000000;
         aclink = Rdatabwsr / Raclink;
         D = Dinternet + Dlan + 120; // access link delay = 2min = 120s
-        printf("Total delay are %fs.\n", D);
+        return D;
     }
 
     else if (a == 1)
@@ -31,6 +51,6 @@ int main()
         Rdatabwsr *= 0.6;
         aclink = Rdatabwsr / Raclink;
         D = 0.6 * (Dinternet + Dlan) + 0.4 * Dcache;
-        printf("Total delay are %fs.\n", D);
+        return D;
     }
 }
